ce63c728d514fe7582d67dc9566028ece6e8dbee
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-11-12 17:42:08 -0700

    all tests passing!

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -1,55 +1,49 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import articleService from './../services/articleServices';
 import View from './View';
-import mockArticleService from '../services/articleServices';
 
-jest.mock('../services/articleServices')
-
-
-const testArr = [ 
-    {
-        id: 'aMqwd', 
-        headline: "headline", 
-        createdOn: Date.time, 
-        summary: "summary", 
-        body: "body",
-        author: "author" 
-    },
-    {
-        id: 'aMqwd', 
-        headline: "headline", 
-        createdOn: Date.time, 
-        summary: "summary", 
-        body: "body",
-        author: "author" 
-    },
-    {
-        id: 'aMqwd', 
-        headline: "headline", 
-        createdOn: Date.time, 
-        summary: "summary", 
-        body: "body",
-        author: "author" 
-    }
-]
-
+jest.mock('./../services/articleServices');
 
 test("renders zero articles without errors", async () => {
-    <View/>
+    articleService.mockResolvedValueOnce({data:[]});
+    
+    render(<View/>);
+    await waitFor(()=> {
+        const articles = screen.queryAllByTestId("article");
+        expect(articles).toHaveLength(0);
+    });
 });
 
 test("renders three articles without errors", async ()=> {
-    mockArticleService.mockResolvedValueOnce(testArr)
+    articleService.mockResolvedValueOnce({data: [
+        {
+            id: 'a', 
+            headline: "headline", 
+            createdOn: Date.time, 
+            summary: "summary", 
+            body: "body",
+            author: "author"
+        },
+        {
+            id: 'b', 
+            headline: "headline", 
+            createdOn: Date.time, 
+            summary: "summary", 
+            body: "body",
+            author: "author"
+        },
+        {
+            id: 'c', 
+            headline: "headline", 
+            createdOn: Date.time, 
+            summary: "summary", 
+            body: "body",
+            author: "author"
+        }
+    ]});
 
-    render(<View/>)
-
-    
-    const articles = screen.queryAllByTestId('showsthree')
-        
-    
-    
-    expect(articles).toHaveLength(0)
+    render(<View />);
+    const articles = await screen.findAllByTestId("article");
+    expect(articles).toHaveLength(3);
 });
-
-//Task List
-//1. Complete the above two tests. Make sure to mocking the articleService call before rendering.
